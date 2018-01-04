7f3dac5afe89a2de738ae1cda3699f49af71ee1d
romans1212notes <romans1212notes@gmail.com>
2018-01-05 07:06:09 +0800

    Add simple beego api

---
+++ bapi/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/romans1212notes/beego-bapi/bapi/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
